c44443d4ae2fff276194d1494b170cedb1108daa
camila <camiramirezpp@gmail.com>
2018-09-12 12:25:54 -0300

    instalacion de firebase,bootstrap y font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +19,11 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALY54w3pacR4MpQ_syWrgp3HJxY9BHURY",
+    authDomain: "celibook-frameworks.firebaseapp.com",
+    databaseURL: "https://celibook-frameworks.firebaseio.com",
+    projectId: "celibook-frameworks",
+    storageBucket: "",
+    messagingSenderId: "448525228438"
+  }
 };
 
 /*

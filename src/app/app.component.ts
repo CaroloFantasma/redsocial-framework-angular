bf931b5788896fe83dc2498e8d2355aee1698572
camila <camiramirezpp@gmail.com>
2018-09-14 13:51:53 -0300

    añadiendo botones en menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import {ViewChild} from '@angular/core';
-import {MatSidenav} from '@angular/material/sidenav';
+import { ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
